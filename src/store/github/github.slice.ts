30b92a9a27bec8f41772979a245c8aec88e86ff2
Nuralla Muzamelov <muzamelovn1@gmail.com>
2022-07-24 12:01:22 +0600

    added project

---
+++ src/store/github/github.slice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface GithubState {
+  favourites: string[];
+}
+
+const initialState: GithubState = {
+  favourites: JSON.parse(localStorage.getItem("favKey") ?? "[]"),
+};
+
+export const githubSlice = createSlice({
+  name: "github",
+  initialState,
+  reducers: {
+    addFavourite(state, action: PayloadAction<string>) {
+      state.favourites.push(action.payload);
+      localStorage.setItem("favKey", JSON.stringify(state.favourites));
+    },
+    removeFavourite(state, action: PayloadAction<string>) {
+      state.favourites = state.favourites.filter((f) => f !== action.payload);
+      localStorage.setItem("favKey", JSON.stringify(state.favourites));
+    },
+  },
+});
+
+export const githubActions = githubSlice.actions;
+export const githubReducer = githubSlice.reducer;
